280f0fa803d594a5ecd38a358750a19cadd2dfa6
choi <sh91cxp@gmail.com>
2022-03-17 00:06:51 +0900

    Throwable 의 error 및 exception 핸들링(복습 필요)

---
+++ src/exceptionHandling/exception_1.java
@@ -0,0 +1,32 @@
+package exceptionHandling;
+
+import java.io.File;
+import java.io.IOException;
+
+public class exception_1 {
+    public static void main(String[] args) {
+        try {
+            File f = createFile("");
+            System.out.println(f.getName() + " 의 파일이 성공적으로 생성되었습니다.");
+        } catch (Exception e) {
+            System.out.println(e.getMessage() + " 다시 입력해주시기 바랍니다.");
+        }
+    }
+
+    static File createFile(String fileName) throws Exception {
+        try {
+            if (fileName==null || fileName.equals(""))
+                throw new Exception("파일 이름이 유효하지 않습니다.");
+        } catch (Exception e) {
+            fileName = "제목_없음.txt";
+        }
+
+        File f = new File(fileName);
+        try {
+            f.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return f;
+    }
+}
